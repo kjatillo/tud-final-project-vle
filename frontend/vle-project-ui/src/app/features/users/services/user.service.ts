29c5c23204c6000db10f8bf76bac05b49f8e8792
Ken <keneithatillo@gmail.com>
2024-11-30 23:35:57 +0000

    feat: setup user service

---
+++ frontend/vle-project-ui/src/app/features/users/services/user.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private apiEndpoint = environment.apiEndpoint;
+
+  constructor(private http: HttpClient) {}
+
+  register(registerData: any): Observable<any> {
+    return this.http.post(`${this.apiEndpoint}/register`, registerData);
+  }
+
+  login(loginData: any): Observable<any> {
+    return this.http.post(`${this.apiEndpoint}/login`, loginData);
+  }
+
+  logout(): Observable<any> {
+    return this.http.post(`${this.apiEndpoint}/logout`, {});
+  }
+}
