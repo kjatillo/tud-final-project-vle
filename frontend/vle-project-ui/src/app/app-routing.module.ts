069d1d3b5a2bf64d5c3ea359aee4e68b4f063a8e
Ken <keneithatillo@gmail.com>
2024-11-30 23:52:50 +0000

    feat(UI): add user registration

--- frontend/vle-project-ui/src/app/app-routing.module.ts
+++ frontend/vle-project-ui/src/app/app-routing.module.ts
@@ -2,8 +2,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './features/users/components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'register', component: RegisterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ frontend/vle-project-ui/src/app/features/users/components/register/register.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../../services/user.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss'],
+})
+export class RegisterComponent {
+  registerForm!: FormGroup;
+
+  constructor(private fb: FormBuilder, private userService: UserService) {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      roleName: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      this.userService.register(this.registerForm.value).subscribe({
+        next: (response) => {
+          console.log('User registed successfully', response);
+        },
+        error: (error) => {
+          console.error('Error registering user', error);
+        },
+      });
+    }
+  }
+}
