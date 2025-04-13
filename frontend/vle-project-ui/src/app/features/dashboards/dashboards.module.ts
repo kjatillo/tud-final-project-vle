0ea76aa73f0fc2a5983c595f7bd6ebe9e6c71f7d
Ken <keneithatillo@gmail.com>
2025-04-13 16:52:47 +0100

    feat(UI): add initial admin dashboard WIP

--- frontend/vle-project-ui/src/app/features/dashboards/dashboards.module.ts
+++ frontend/vle-project-ui/src/app/features/dashboards/dashboards.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StudentDashboardComponent } from './components/student-dashboard/student-dashboard.component';
 import { InstructorDashboardComponent } from './components/instructor-dashboard/instructor-dashboard.component';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 
 @NgModule({
   declarations: [
+    AdminDashboardComponent,
     InstructorDashboardComponent,
     StudentDashboardComponent
   ],
@@ -12,6 +14,7 @@ import { InstructorDashboardComponent } from './components/instructor-dashboard/
     CommonModule,
   ],
   exports: [
+    AdminDashboardComponent,
     InstructorDashboardComponent,
     StudentDashboardComponent
   ]
