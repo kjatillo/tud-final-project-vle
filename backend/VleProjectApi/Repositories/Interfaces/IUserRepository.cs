cc450b16742a56d2e9189d82233201002f1f8ff1
Ken <keneithatillo@gmail.com>
2025-04-15 15:32:40 +0100

    feat(API): get module participants

--- backend/VleProjectApi/Repositories/Interfaces/IUserRepository.cs
+++ backend/VleProjectApi/Repositories/Interfaces/IUserRepository.cs
@@ -5,4 +5,5 @@ namespace VleProjectApi.Repositories.Interfaces;
 public interface IUserRepository
 {
     Task<IEnumerable<ApplicationUser>> GetInstructorsAsync();
+    Task<IEnumerable<ApplicationUser>> GetEnroledUsersByModuleIdAsync(Guid moduleId);
 }
