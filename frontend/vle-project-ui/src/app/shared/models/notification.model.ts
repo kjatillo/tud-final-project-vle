83a20c6a536c5d3725ef7f43661ad9c0777aecf1
Ken <keneithatillo@gmail.com>
2025-04-22 17:14:13 +0100

    feat(UI/notification): generalise notification and add admin support

--- frontend/vle-project-ui/src/app/shared/models/notification.model.ts
+++ frontend/vle-project-ui/src/app/shared/models/notification.model.ts
@@ -1,10 +1,11 @@
 export interface Notification {
     id: string;
     message: string;
-    moduleId: string;
+    moduleId?: string;
     moduleTitle?: string;
     createdAt: Date;
     isRead: boolean;
     tempId?: boolean; // Flag to identify client-generated IDs
+    notificationType?: string;
   }
   
